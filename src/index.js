7f9baad58a3a73a615f4a992aec14660d8c147ff
svraj777 <41935749+svraj777@users.noreply.github.com>
2021-04-03 12:16:22 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
+// /*https://run.mocky.io/v3/aa4d7762-6fd9-45aa-9ba3-51d89b8a722f*/
+// /*https://designer.mocky.io/manage/delete/aa4d7762-6fd9-45aa-9ba3-51d89b8a722f/j0I5U7B2dbCtmNoXTGukFACfgM8mTH7hQOzA*/
